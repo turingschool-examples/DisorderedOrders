d2a4b5c10351aa66a79196929768cf0125aa0b92
jkimball54 <jeremyck54@gmail.com>
2023-10-16 15:10:42 -0600

    Order Create action abstractions

--- DisorderedOrdersMVC/Controllers/OrdersController.cs
+++ DisorderedOrdersMVC/Controllers/OrdersController.cs
@@ -28,61 +28,16 @@ namespace DisorderedOrdersMVC.Controllers
         public IActionResult Create(IFormCollection collection, string paymentType)
         {
             // create order
-            int customerId = Convert.ToInt16(collection["CustomerId"]);
-            Customer customer = _context.Customers.Find(customerId);
-            var order = new Order() { Customer = customer };
-            for (var i = 1; i < collection.Count - 1; i++)
-            {
-                var kvp = collection.ToList()[i];
-                if (kvp.Value != "0")
-                {
-                    var product = _context.Products.Where(p => p.Name == kvp.Key).First();
-                    var orderItem = new OrderItem() { Item = product, Quantity = Convert.ToInt32(kvp.Value) };
-                    order.Items.Add(orderItem);
-                }
-            }
-
+            var order = createOrder(collection, _context);
             // verify stock available
-            foreach (var orderItem in order.Items)
-            {
-                if (!orderItem.Item.InStock(orderItem.Quantity))
-                {
-                    orderItem.Quantity = orderItem.Item.StockQuantity;
-                }
-
-                orderItem.Item.DecreaseStock(orderItem.Quantity);
-            }
-
+            verifyStock(order);
             // calculate total price
-            var total = 0;
-            foreach (var orderItem in order.Items)
-            {
-                var itemPrice = orderItem.Item.Price * orderItem.Quantity;
-                total += itemPrice;
-            }
-
+            var total = calculateTotalPrice(order);
             // process payment
-            IPaymentProcessor processor;
-            if (paymentType == "bitcoin")
-            {
-                processor = new BitcoinProcessor();
-            }
-            else if (paymentType == "paypal")
-            {
-                processor = new PayPalProcessor();
-            }
-            else
-            {
-                processor = new CreditCardProcessor();
-            }
+            processPayment(_context, order, total, paymentType);
 
-            processor.ProcessPayment(total);
-
-            _context.Orders.Add(order);
-            _context.SaveChanges();
-
-            return RedirectToAction("Show", new { id = order.Id});
-        }
+			return RedirectToAction("Show", new { id = order.Id });
+		}
 
         [Route("/orders/{id:int}")]
         public IActionResult Show(int id)
@@ -103,5 +58,70 @@ namespace DisorderedOrdersMVC.Controllers
 
             return View(order);
         }
-    }
+
+        //create order
+        static Order createOrder(IFormCollection collection, DisorderedOrdersContext _context)
+        {
+			int customerId = Convert.ToInt16(collection["CustomerId"]);
+			Customer customer = _context.Customers.Find(customerId);
+			var order = new Order() { Customer = customer };
+			for (var i = 1; i < collection.Count - 1; i++)
+			{
+				var kvp = collection.ToList()[i];
+				if (kvp.Value != "0")
+				{
+					var product = _context.Products.Where(p => p.Name == kvp.Key).First();
+					var orderItem = new OrderItem() { Item = product, Quantity = Convert.ToInt32(kvp.Value) };
+					order.Items.Add(orderItem);
+				}
+			}
+            return order;
+		}
+        //verify stock available
+        static void verifyStock(Order order)
+        {
+			foreach (var orderItem in order.Items)
+			{
+				if (!orderItem.Item.InStock(orderItem.Quantity))
+				{
+					orderItem.Quantity = orderItem.Item.StockQuantity;
+				}
+
+				orderItem.Item.DecreaseStock(orderItem.Quantity);
+			}
+		}
+        //calculate total price
+        static int calculateTotalPrice(Order order)
+        {
+			var total = 0;
+			foreach (var orderItem in order.Items)
+			{
+				var itemPrice = orderItem.Item.Price * orderItem.Quantity;
+				total += itemPrice;
+			}
+            return total;
+		}
+		//process payment
+		static void processPayment(DisorderedOrdersContext _context, Order order, int total, string paymentType)
+		{
+            IPaymentProcessor processor;
+            if (paymentType == "bitcoin")
+            {
+                processor = new BitcoinProcessor();
+            }
+            else if (paymentType == "paypal")
+            {
+                processor = new PayPalProcessor();
+            }
+            else
+            {
+                processor = new CreditCardProcessor();
+            }
+
+            processor.ProcessPayment(total);
+
+            _context.Orders.Add(order);
+            _context.SaveChanges();
+		}
+	}
 }
